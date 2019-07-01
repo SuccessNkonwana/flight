e1066200bb5e2f7e7d5bef5ad0e99149f244c2f9
success <nkonwanasuccess@gmail.com>
2019-07-01 06:08:40 -0700

    flight booking app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlightComponent } from './flight/flight.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:FlightComponent},
+  {path:'payment',component:PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightComponent } from './flight/flight.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
